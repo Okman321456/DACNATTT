3ccc33e3f19e3fffb74fdac15d4446e5810e2bed
Pham Huu Hoang <hoangpham2k1608@gmail.com>
2022-01-17 14:51:38 +0700

    add getTicketPerPhone/PerDate

--- backend/src/routes/ticket.route.js
+++ backend/src/routes/ticket.route.js
@@ -1,16 +1,18 @@
-const express = require('express');
-const auth = require('../middlewares/auth')
-const ticketController = require('../controllers/ticket.controller')
+const express = require("express");
+const auth = require("../middlewares/auth");
+const ticketController = require("../controllers/ticket.controller");
 
 const router = express.Router();
 
-router.post('/book/:tourId', ticketController.bookTicket)
-router.get('/tour/:idTour', ticketController.showTicketPerTour)
-router.get('/detail/:ticketId', auth('admin', 'manage'), ticketController.viewDetailTicket)
-router.delete('/delete/:ticketId', auth('manage'), ticketController.deleteTicket)
-router.put('/updateStatus/:ticketId/:ticketStatus', auth('manage'), ticketController.updateTicketStatus)
-router.get('/listTicket', auth('admin', 'manage'), ticketController.viewAllTicket)
-router.get('/region/:idRegion', ticketController.getTicketRegion)
-router.get('/sort', ticketController.sortTicket)
+router.post("/book/:tourId", ticketController.bookTicket);
+router.get("/tour/:idTour", ticketController.showTicketPerTour);
+router.get("/detail/:ticketId", auth("admin", "manage"), ticketController.viewDetailTicket);
+router.delete("/delete/:ticketId", auth("manage"), ticketController.deleteTicket);
+router.put("/updateStatus/:ticketId/:ticketStatus", auth("manage"),ticketController.updateTicketStatus);
+router.get("/listTicket", auth("admin", "manage"), ticketController.viewAllTicket);
+router.get("/region/:idRegion", ticketController.getTicketRegion);
+router.get("/sort", ticketController.sortTicket);
+router.get("/phone/:phone", ticketController.showTicketPerPhone);
+router.get("/date/:date", ticketController.showTicketPerDate);
 
 module.exports = router;
